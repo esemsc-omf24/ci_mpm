7371821cf667136bd236b7505a3beae120805d64
esemsc-omf24 <omf24@ic.ac.uk>
2024-10-15 11:58:17 +0100

    Added factorial and tests

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,5 +1,11 @@
+import functools
 
-__all__ = ['my_sum']
+__all__ = ["my_sum", "factorial"]
+
+
+@functools.cache
+def factorial(n):
+    return n * factorial(n - 1) if n > 1 else 1
 
 
 def my_sum(iterable):
